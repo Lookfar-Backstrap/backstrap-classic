6b20d628b55eaf97ce0d7078baca7cf4406a7e64
Jonathan Biguenet <jonnyb@lookfarlabs.com>
2024-12-05 13:28:19 -0500

    - updating config files in user_files for starting new projects

--- user_files/config/config.development.js
+++ user_files/config/config.development.js
@@ -1,12 +1,17 @@
 module.exports = {
   db: {
-    user: process.env.DB_USER || '[YOUR DB USER]',
-    name: process.env.DB_NAME || '[YOUR DB NAME]',
-    pass: process.env.DB_PASS || '[YOUR DB PASSWORD]',
+    user: process.env.DB_USER || '[YOUR DB USER HERE]',
+    name: process.env.DB_NAME || '[YOUR DB NAME HERE]',
+    pass: process.env.DB_PASS || '[YOUR DB PASSWORD HERE]',
     host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || '5432'
+    port: process.env.DB_PORT || '5432',
+    ssl: {
+      ca: process.env.DB_SSL_CA || null,
+      key: process.env.DB_SSL_KEY || null,
+      cert: process.env.DB_SSL_CERT || null
+    }
   },
   s3: {
-  	bucket: '[YOUR BUCKET]'
+    bucket: '[YOUR BUCKET HERE]'
   }
 };
--- user_files/config/config.local.js
+++ user_files/config/config.local.js
@@ -1,12 +1,12 @@
 module.exports = {
   db: {
-    user: process.env.DB_USER || '[YOUR DB USER]',
-    name: process.env.DB_NAME || '[YOUR DB NAME]',
-    pass: process.env.DB_PASS || '[YOUR DB PASSWORD]',
+    user: process.env.DB_USER || '[YOUR DB USER HERE]',
+    name: process.env.DB_NAME || '[YOUR DB NAME HERE]',
+    pass: process.env.DB_PASS || '[YOUR DB PASSWORD HERE]',
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || '5432'
   },
   s3: {
-    bucket: '[YOUR BUCKET]'
+    bucket: '[YOUR BUCKET HERE]'
   }
 };
